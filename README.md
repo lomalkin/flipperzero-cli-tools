ccac53238ac10b56bdf2a7e7ed1495d609969b8f
Lesha Lomalkin <info@@lomalkin.ru>
2022-08-09 01:57:40 +0300

    Work via protobuf-RPC, some first commands, rempve legacy cli screen* cmds

--- README.md
+++ README.md
@@ -5,15 +5,41 @@
 * $ `sudo apt install python3 python3-venv python3-pip`
 * $ `git clone https://github.com/lomalkin/flipperzero-cli-tools`
 * $ `cd flipper-cli-tools`
+* $ `git submodule update --init --recursive`
 * $ `python3 -m venv venv`
 * $ `pip install -r requirements.txt`
 
 ## Usage
 
+**Preparing**
+
 * $ `. venv/bin/activate` - to activate python virtual environment
 * $ `deactivate` - to deactivate
 
-### Tools
+**Command line arguments**
 
-* `./screen /dev/tty<path to Flipper serial>` - Make screenshot directly to terminal and save to `screen.png`.
-* `./screen_stream /dev/tty<path to Flipper serial>` - Screen streaming directly to terminal (need UTF support).
+```./rpc.py <Flipper> [commands]```
+* `Flipper` - name of your Flipper or full path to Flipper serial in your OS.
+* `commands` - list of spaced short command aliases, see below:
+
+### Command aliases
+* `ok`, `bk`, `up`, `dn`, `lt`, `rt` - Key control:  Ok, Back, Up, Down, Left, Right
+* `ping` - ping-pong
+* `alert` - play built-in vibro-audio-visual alert
+* `screen` - dump screen image to console
+* `exit` - exit from RPC app
+* `s1` - sleep for 1 sec between commands
+
+#### Examples
+
+1. Dump screenshot to console
+
+```./rpc.py Lotak screen```
+
+2. Run music_player app via keyboard from default state, wait 5 seconds and exit:
+
+```./rpc.py Lotak ok dn dn dn dn dn dn dn dn ok dn ok dn ok s1 s1 s1 s1 s1 bk bk bk```
+
+3. Play alert on your Flipper
+
+```./rpc.py Lotak alert```
---
+++ rpc.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import sys, os, time
+import serial
+
+from flipperzero_protobuf_py.flipper_protobuf import ProtoFlipper
+from flipperzero_protobuf_py.cli_helpers import *
+
+def flp_exec_cmd(proto, cmd):
+    if cmd == 'alert':
+        proto.cmd_system_audiovisual_alert()
+    elif cmd == 'ping':
+        if proto.cmd_system_ping() == b'\xde\xad\xbe\xef':
+            print('pong')
+        else:
+            print('ping error', file=sys.stderr)
+    elif cmd == 'up':
+        proto.cmd_gui_send_input("SHORT UP")
+    elif cmd == 'dn':
+        proto.cmd_gui_send_input("SHORT DOWN")
+    elif cmd == 'lt':
+        proto.cmd_gui_send_input("SHORT LEFT")
+    elif cmd == 'rt':
+        proto.cmd_gui_send_input("SHORT RIGHT")
+    elif cmd == 'ok':
+        proto.cmd_gui_send_input("SHORT OK")
+    elif cmd == 'bk':
+        proto.cmd_gui_send_input("SHORT BACK")
+    elif cmd == 'screen':
+        print_screen(proto.cmd_gui_snapshot_screen())
+    elif cmd == 'exit':
+        proto.cmd_app_exit()
+    elif cmd == 's1':
+        time.sleep(1)
+    else:
+        print("Unknown command " + cmd, file=sys.stderr)
+
+def flp_exec_cmds(proto, cmds):
+    for cmd in cmds:
+        flp_exec_cmd(proto, cmd)
+
+def flp_serial_by_name(flp_name):
+    if sys.platform == 'darwin':    #MacOS
+        flp_serial = '/dev/cu.usbmodemflip_' + flp_name + '1'
+    elif sys.platform == 'linux':   #Linux
+        flp_serial = '/dev/serial/by-id/usb-Flipper_Devices_Inc._Flipper_' + flp_name + '_flip_' + flp_name + '-if00'
+
+    if os.path.exists(flp_serial):
+        return flp_serial
+    else:
+        if os.path.exists(flp_name):
+            return flp_name
+        else:
+            return ''
+
+def main():
+    flp_serial = flp_serial_by_name(sys.argv[1])
+
+    if flp_serial == '':
+        print("Name or serial port is invalid")
+        sys.exit(1)
+    
+    flipper = serial.Serial(flp_serial, timeout=1)
+    flipper.baudrate = 230400
+    flipper.flushOutput()
+    flipper.flushInput()
+
+    flipper.timeout = None
+
+    flipper.read_until(b'>: ')
+
+    flipper.write(b"start_rpc_session\r")
+    flipper.read_until(b'\n')
+
+    proto = ProtoFlipper(flipper)
+
+    flp_exec_cmds(proto, sys.argv[2:])
+
+if __name__ == '__main__':
+    main()
